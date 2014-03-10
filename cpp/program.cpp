b315b49007a818ca04ed8c6abaeb8c1adf17aaef
eriknylander <nylander.e@gmail.com>
2014-03-10 16:13:40 +0100

    Trying smart solution for starting bytes

--- cpp/program.cpp
+++ cpp/program.cpp
@@ -18,6 +18,30 @@ using namespace std;
 						0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x7b, 
 						0x7c, 0x7d, 0x7e, 0x7f, 0x80, 0x81, 0x82, 0x83, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e};*/
 
+int prefixMask = 0xFF0000;
+int firstOpcodeMask = 0x00FF00;
+int secondOpcodeMask = 0x0000FF;
+
+struct Opcode 
+{
+	Opcode(int opcode) 
+	{
+		if(opcode > 0x00FFFF) {
+			bytes.push_back(opcode & secondOpcodeMask);
+			bytes.push_back((opcode & firstOpcodeMask) >> 2);
+			bytes.push_back((opcode & prefixMask) >> 4);
+		} else if(opcode > 0x0000FF) {
+			bytes.push_back(opcode & secondOpcodeMask);
+			bytes.push_back((opcode & firstOpcodeMask) >> 2);
+		} else {
+			bytes.push_back(opcode & secondOpcodeMask);
+		}
+	}
+
+	vector<BYTE> bytes;
+};
+
+
 bool InstructionCompare(const struct emu_cpu_instruction &a, const struct emu_cpu_instruction &b)
 {
 	return (a.opc == b.opc) && (a.modrm.mod == b.modrm.mod) && (a.modrm.opc == b.modrm.opc) && (a.modrm.sib.base == b.modrm.sib.base)
